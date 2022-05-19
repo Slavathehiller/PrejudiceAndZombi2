7cc5f9b3baec32150c6c5b8f2bdb0a507cba3d94
Slavathehiller <hiller@tut.by>
2022-05-19 22:03:54 +0300

    Реализация стратегической карты

---
+++ Assets/Scripts/Srategic/Sector.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+
+    public GameController gameController;
+    private GameObject selector;
+    private GameObject cameraContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selector = gameController.selector;
+        cameraContainer = gameController.cameraContainer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameController.cameraMoving)
+            return;
+        selector.transform.SetParent(gameObject.transform);
+        selector.transform.localPosition = Vector3.zero;
+        StartCoroutine(MoveCamera(gameObject.transform.position));
+    }
+
+    IEnumerator MoveCamera(Vector3 point)
+    {
+
+        while(Vector3.Distance(cameraContainer.transform.position, point) > 0.01f)
+        {
+            gameController.cameraMoving = true;
+            var movePoint = Vector3.MoveTowards(cameraContainer.transform.position, point, 0.01f);
+            cameraContainer.transform.position = movePoint;
+            yield return new WaitForSeconds(0.01f);
+        }
+        gameController.cameraMoving = false;
+    }
+
+}
